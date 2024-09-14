549c4bad34daa4ab6c7e506050ce003de229e7f0
AhmedRaouf481 <araouf481@hotmail.com>
2024-09-14 21:47:40 +0300

    make filters in get slots

--- server/src/main/java/com/clinicare/server/repository/SlotRepository.java
+++ server/src/main/java/com/clinicare/server/repository/SlotRepository.java
@@ -1,6 +1,7 @@
 package com.clinicare.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.clinicare.server.domain.db.Slot;
 import com.clinicare.server.domain.response.SlotProjection;
@@ -8,7 +9,9 @@ import com.clinicare.server.domain.response.SlotProjection;
 import java.util.List;
 
 
-public interface SlotRepository extends JpaRepository<Slot,Long>{
+public interface SlotRepository extends JpaRepository<Slot,Long>,JpaSpecificationExecutor<Slot>{
     
     List<SlotProjection> findByDoctorId(Long doctorId);
+
+
 }
